e3402c6b92c506f4924c5dfc408fe6d576f34a1a
hossein <143866200+hos3sein@users.noreply.github.com>
2023-10-23 22:59:50 +0330

    Add files via upload

---
+++ mouse control.py
@@ -0,0 +1,52 @@
+from pynput.mouse import Button , Controller
+from pynput import keyboard
+
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# mouse = Controller()
+
+# firstposition = format(mouse.poseition)
+
+# mouse.poseition(10,20)
+
+# mouse.move(5,-5)
+
